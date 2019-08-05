c2f6d88fbadba464f4e7f6ea8701c80813710541
jpibernat <joaquin.pibernat@gmail.com>
2019-08-04 21:13:35 -0300

    navbar para ecommerce dh

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <ul class="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Preguntas</a></li>
+            <li><a href="index.php"><img src="img/logo.png" alt=""></a></li>
+            <li><a href="#">Registrate</a></li>
+            <li><a href="#">Login</a></li>
+        </ul>
+        <div class="logo">
+            <img src="img/logo.png" alt=""></a>
+        </div>
+       <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+         </div>
+         <!--<label for="toggle">&#9776;</label>
+         <input type="checkbox" id="toggle"/>-->
+    </nav>
+    <div class="container">
+
+    </div>
+<script src="js/nav.js"></script>
+</body>
+</html>
